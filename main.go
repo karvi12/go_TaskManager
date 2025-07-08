27883191aed441d9c97fd8507c9ab4e53a8a253b
karvi12 <kiri11.1w@yandex.ru>
2025-07-08 09:52:38 +0300

    Починил ввод строки с пробелами, теперь задачи правильно добавляются в массив Task

--- main.go
+++ main.go
@@ -1,43 +1,55 @@
 package main
 
 import (
+	"bufio"
 	. "fmt"
+	"os"
+	"strings"
 	. "time"
 )
+
 type Task struct {
-		ID   int
-		Text string
-		Done bool
+	ID   int
+	Text string
+	Done bool
 }
 
-func printTask(t Task){
-		Sleep(2*Second)
-		Print("\n", "ID: ", t.ID)
-		Sleep(2*Second)
-		Print("\n", "Задание: ", t.Text)
-		Sleep(2*Second)
-		Print("\n", "Статус: ", t.Done, "\n")
+func printTask(t Task) {
+	Print("\n", "ID: ", t.ID)
+	Print("\n", "Задание: ", t.Text)
+	Print("\n", "Статус: ", t.Done, "\n")
 }
-func inputTask(id int, name string, status bool) Task{
-	task := Task{id, name, status}
-	return task
+func inputTask(id int, name string, status bool) Task {
+	return Task{id, name, status}
 }
 
 func main() {
-	var name, text string
+	var name string
 	var id int
-	var status bool
+	var status string
+
+	reader := bufio.NewReader(os.Stdin)
+
 	Print("Введите ваше имя: ")
 	Scan(&name)
 	Print("Привет, ", name, "! Это наш менеджер задач")
 	Print("\n", Now().Year(), "-", Now().Month(), "-", Now().Day())
 	Print("\n", Now().Hour(), ":", Now().Minute(), ":", Now().Second(), "\n")
-	Print("Введите информацию о вашей задаче: ")
-	Scan(&id, &text, &status)
-	tasks := make([]Task, 10)
-	tasks[0] = inputTask(id, text, status)
-	tasks = append(tasks, Task{0, "Покушать", false})
+
+	Print("Введите ID задачи: ")
+	Scan(&id)
+
+	reader.ReadString('\n')
+
+	Print("Введите описание задачи: ")
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
+	Print("Введите статус задачи (true/false): ")
+	Scan(&status)
+
+	statusBool := status == "true"
+
+	tasks := []Task{}
+	tasks = append(tasks, inputTask(id, text, statusBool))
 	printTask(tasks[0])
-	// printTask(tasks[1])
-	Print(tasks[1])
 }
